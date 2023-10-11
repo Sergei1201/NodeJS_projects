01d1eeb92f5e275e38e5265447fd0e48884d8a43
Sergei Sokolov <sokolov12ssa@gmail.com>
2023-10-11 23:39:47 +0500

    displaying stories

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const exphbs = require('express-handlebars')
 const path = require('path')
 const passport = require('passport')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const MongoStore = require('connect-mongo')
 
 // Config
@@ -22,20 +23,31 @@ const app = express()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+// Method override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 // Use morgan if in the development mode
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('short'))
 }
 
 // Handlebars helpers
-const {formatDate, truncate, stripTags, editIcon} = require('./helpers/hbs')
+const {formatDate, truncate, stripTags, editIcon, select} = require('./helpers/hbs')
 
 // Handlebars
 app.engine('.hbs', exphbs.engine({helpers: {
   formatDate,
   truncate,
   stripTags,
-  editIcon},
+  editIcon,
+  select},
   defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
--- routes/stories.js
+++ routes/stories.js
@@ -5,29 +5,90 @@ const Story = require('../models/Story')
 
 // Add Story (GET)
 router.get('/add', ensureAuth, (req, res) => {
-    res.render('stories/add')
+    try {
+        res.render('stories/add')
+    } catch (error) {
+        console.error(error)
+        res.render('errors/500')
+    }
 })
 
 // Edit Story (GET)
 router.get('/edit/:id', ensureAuth, async (req, res) => {
     // Match the database user against the URL user
-    let story = await Story.findOne({
-        _id: req.params.id
-    }).lean()
+    try {
+        let story = await Story.findOne({
+            _id: req.params.id
+        }).lean()
+    
+        if (!story) {
+            return res.render('errors/404')
+        }
+        if (story.user != req.user._id) {
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', {
+                story
+            })
+        }
+    }
+    catch (error) {
+        console.error(error)
+        res.render('errors/404')
+    }
+}
+    )
 
-    if (!story) {
-        return res.render('errors/404')
-    }
-    if (story.user != req.user._id) {
+// Get Single Story
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        // Find Story By ID
+        let story = await Story.findById({_id: req.params.id})
+                    .populate('user')
+                    .lean()
+        if (!story) {
+            return res.render('errors/404')
+        }
+        
+        // Make sure that the user owns a particular story and the story is not private to show
+        if (story.user != req.user._id && story.status == 'private') {
+            res.render('errors/404')
+        } else {
+            res.render('stories/show', {
+                story
+            })
+            console.log(story)
+        }
+        
+    } catch (error) {
+        console.error(error)
         res.redirect('/stories')
-    } else {
-        res.render('stories/edit', {
-            story
-        })
-        console.log(story)
+        
     }
+
 })
 
+// Get All User's Stories
+router.get('/user/:userID', ensureAuth, async (req, res) => {
+    try {
+        let stories = await Story.find({user: req.params.userID, status: 'public'})
+                                        .populate('user')
+                                        .lean()
+        if (!stories) {
+            return res.render('errors/404')
+        } else {
+            res.render('stories/index', {
+                stories
+            })
+        }
+        
+    } catch (error) {
+        console.error(error)
+        res.render('/dashboard')
+        
+    }
+})
+   
 // Add Story (POST)
 router.post('/', ensureAuth, async (req, res) => {
     try {
@@ -43,6 +104,7 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 
 })
+
 // Get Public Stories
 router.get('/', ensureAuth, async (req, res) => {
     // Fetch public stories from DB and pass them to template
@@ -59,6 +121,46 @@ router.get('/', ensureAuth, async (req, res) => {
         console.error(error)
     }
 })
+
+// Update Story (PUT)
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+
+    // Find a story from DB that matches the story from the URl
+        let story = await Story.findById({_id: req.params.id}).lean()
+        if (!story) {
+            return res.render('errors/404')
+        }
+    // Make sure that the user owns theri story in order to update it
+        if (story.user != req.user._id) {
+            res.redirect('/stories')
+        } else {
+    // Find the story by ID and update
+            story = await Story.findByIdAndUpdate({_id: req.params.id}, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.redirect('/dashboard')
+        }
+    } catch (error) {
+        console.error(error)
+        res.redirect('errors/500')
+    }
+})
+        
+// Delete Story (DELETE)
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        // Find by ID in the database and remove
+        await Story.findByIdAndRemove({_id: req.params.id})
+        res.redirect('/dashboard')
+        
+    } catch (error) {
+        console.error(error)
+        res.render('errors/500')
+    }
+})
+   
 
 
 module.exports = router
